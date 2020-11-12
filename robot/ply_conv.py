f89a47b580f6153b21915a3e8fba077c2f678e1b
Harish <kuppamharish28@gmail.com>
2020-11-12 12:39:30 -0500

    model_final

--- robot/ply_conv.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from plyfile import PlyData, PlyElement
-plydata = PlyData.read('model1.ply')
-el = PlyData([plydata.elements[0],plydata.elements[1]],text=True)
-#PlyData([el], text=True).write('some_ascii.ply')
-test_file = './some_ascii.ply'
-with open(test_file,'wb') as f:
-    el.write(f)
